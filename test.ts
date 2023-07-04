9fcf3b35749613d357dcd841d74dc2b5cc50401f
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-07-04 20:37:49 -0300

    :fu:

---
+++ test.ts
@@ -0,0 +1,28 @@
+class Car {
+    private name:String;
+    private color:String;
+    private age:number;
+
+
+    public setName(name: String): void {
+        this.name = name;
+    }
+
+    public setColor(color: String): void {
+        this.color = color;
+    }
+
+    public setAge(age: number): void {
+        this.age = age;
+    }
+
+    public getInfo(): void {
+        console.log(`Nome: ${this.name}, Color: ${this.color}, Age: ${this.age}`);
+    }    
+}
+
+const myCar = new Car();
+myCar.setName("Honda Civic Si");
+myCar.setColor("Preto");
+myCar.setAge(2009);
+myCar.getInfo();
